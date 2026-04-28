79fe3efa34802bfc4c78129bdbee21eee3ddbdb6
User_087 <WP_SL_User_087@tekcloudlabsoutlook.onmicrosoft.com>
2026-04-28 16:58:10 +0530

    Sixth

--- Frontend/fleet-management-app/src/app/app.routes.ts
+++ Frontend/fleet-management-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Register } from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Login},
+  
+      { path: 'login', redirectTo: '', pathMatch: 'full' },
+          {path:'dashboard',component:Dashboard},
+              {path:'register',component:Register},
+  { path: '**', redirectTo: '' }
+];
